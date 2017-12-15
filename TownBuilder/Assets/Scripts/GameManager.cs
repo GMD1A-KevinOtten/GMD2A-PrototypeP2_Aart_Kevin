962cc83d68fbe8eef45000ef7db3fe8089d6c838
Kevin Otten <kevin.otten@ziggo.nl>
2017-12-15 13:35:15 +0100

    menu

---
+++ TownBuilder/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager gm;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        if (gm == null)
+        {
+            gm = this;
+        }
+        else if (gm != null && gm != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public static void SceneChange(int index)
+    {
+        SceneManager.LoadScene(index, LoadSceneMode.Single);
+    }
+}
---
+++ TownBuilder/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    public void Quit()
+    {
+        GameManager.QuitGame();
+    }
+
+    public void Play()
+    {
+        GameManager.SceneChange(1);
+    }
+}
