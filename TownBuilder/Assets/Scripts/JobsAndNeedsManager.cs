d55abe71a44b7f1ca88c04a45d2adf560f402c5c
Kevin Otten <kevin.otten@ziggo.nl>
2018-01-08 17:51:53 +0100

    click en Shift click selectie op resources dat kan worden gekombineerd met ene ui button om die objecten toetevoegen aan de toHarvest list (klein errortje dat ik morgen ga fixen)

--- TownBuilder/Assets/Scripts/JobsAndNeedsManager.cs
+++ TownBuilder/Assets/Scripts/JobsAndNeedsManager.cs
@@ -5,4 +5,79 @@ using UnityEngine;
 public class JobsAndNeedsManager : MonoBehaviour {
 
     public static List<GameObject> toBuild = new List<GameObject>();
+    public static List<GameObject> toHarvest = new List<GameObject>();
+    public List<GameObject> selectedObjects = new List<GameObject>();
+
+    void Update()
+    {
+        SelectIntObject();
+    }
+
+    public void SelectIntObject()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.GetComponent<RecoursHolder>())
+                {
+                    if(Input.GetButton("LShift"))
+                    {
+                        print("ok");
+                        selectedObjects.Add(hit.transform.gameObject);
+                        hit.transform.gameObject.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+                    }
+                    else
+                    {
+                        foreach (GameObject gameobject in selectedObjects)
+                        {
+                            gameobject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+                        }
+                        selectedObjects.Clear();
+                        selectedObjects.Add(hit.transform.gameObject);
+                        hit.transform.gameObject.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+                        print(selectedObjects);
+                    }
+                }
+                else if(hit.transform.tag != "Canvas")
+                {
+                    foreach (GameObject gameobject in selectedObjects)
+                    {
+                        print("Wut");
+                        gameobject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+                        selectedObjects.Clear();
+                    }
+                }
+            }
+        }
+    }
+
+    public void AddToHarvestList()
+    {
+        foreach(GameObject gameobject in selectedObjects)
+        {
+            if(toHarvest.Count != 0)
+            {
+                foreach (GameObject harvestque in toHarvest)
+                {
+                    if (harvestque != gameobject)
+                    {
+                        toHarvest.Add(gameobject);
+                        print(toHarvest[1]);
+                    }
+                }
+            }
+            else
+            {
+                toHarvest.Add(gameobject);
+                print(toHarvest[0]);
+            }
+        }
+        foreach (GameObject gameobject in selectedObjects)
+        {
+            gameobject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        }
+    }
 }
---
+++ TownBuilder/Assets/Scripts/Recourses.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recourses : ScriptableObject
+{
+    public int wood;
+    public int stone;
+    public int metal;
+    public int food;
+}
