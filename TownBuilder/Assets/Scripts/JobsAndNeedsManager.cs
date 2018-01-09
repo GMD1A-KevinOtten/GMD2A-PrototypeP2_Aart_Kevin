e8781d3a7ab42f4a6917a68eaab973678466f85f
Kevin Otten <kevin.otten@ziggo.nl>
2018-01-09 15:44:27 +0100

    slecting recources klaar alleen nog box select invoegen als me dat tijdig lukt

--- TownBuilder/Assets/Scripts/JobsAndNeedsManager.cs
+++ TownBuilder/Assets/Scripts/JobsAndNeedsManager.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+
 
 public class JobsAndNeedsManager : MonoBehaviour {
 
     public static List<GameObject> toBuild = new List<GameObject>();
     public static List<GameObject> toHarvest = new List<GameObject>();
     public List<GameObject> selectedObjects = new List<GameObject>();
+    public bool overButon;
 
     void Update()
     {
         SelectIntObject();
+    }
+
+    public void OverButonTrigger()
+    {
+        if(overButon)
+        {
+            overButon = false;
+        }
+        else
+        {
+            overButon = true;
+        }
     }
 
     public void SelectIntObject()
@@ -25,30 +40,27 @@ public class JobsAndNeedsManager : MonoBehaviour {
                 {
                     if(Input.GetButton("LShift"))
                     {
-                        print("ok");
                         selectedObjects.Add(hit.transform.gameObject);
                         hit.transform.gameObject.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
                     }
                     else
                     {
-                        foreach (GameObject gameobject in selectedObjects)
+                        foreach (GameObject g in selectedObjects)
                         {
-                            gameobject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+                            g.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
                         }
                         selectedObjects.Clear();
                         selectedObjects.Add(hit.transform.gameObject);
                         hit.transform.gameObject.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
-                        print(selectedObjects);
                     }
                 }
-                else if(hit.transform.tag != "Canvas")
+                else if (EventSystem.current.IsPointerOverGameObject() && overButon == false)
                 {
-                    foreach (GameObject gameobject in selectedObjects)
+                    foreach(GameObject gameobject in selectedObjects)
                     {
-                        print("Wut");
                         gameobject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
-                        selectedObjects.Clear();
                     }
+                    selectedObjects.Clear();
                 }
             }
         }
@@ -56,28 +68,23 @@ public class JobsAndNeedsManager : MonoBehaviour {
 
     public void AddToHarvestList()
     {
-        foreach(GameObject gameobject in selectedObjects)
-        {
-            if(toHarvest.Count != 0)
-            {
-                foreach (GameObject harvestque in toHarvest)
-                {
-                    if (harvestque != gameobject)
-                    {
-                        toHarvest.Add(gameobject);
-                        print(toHarvest[1]);
-                    }
-                }
-            }
-            else
-            {
-                toHarvest.Add(gameobject);
-                print(toHarvest[0]);
-            }
-        }
         foreach (GameObject gameobject in selectedObjects)
         {
+            bool alreadyIn = false;
+            foreach (GameObject harvestque in toHarvest)
+            {
+                if(harvestque == gameobject)
+                {
+                    alreadyIn = true;
+                }
+            }
+            if(alreadyIn == false)
+            {
+                toHarvest.Add(gameobject);
+            }
             gameobject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+
         }
+        selectedObjects.Clear();
     }
 }
